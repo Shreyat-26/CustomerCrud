2785d41eb96f957f85d86808a84d25cd92075df0
Shreya Tewari <shreyatewari95@gmail.com>
2021-04-09 01:53:41 +0530

    added update post and delete operations

--- src/main/java/com/demo/crud/rest/CustomerResource.java
+++ src/main/java/com/demo/crud/rest/CustomerResource.java
@@ -1,11 +1,18 @@
 package com.demo.crud.rest;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.demo.crud.models.Customer;
@@ -20,15 +27,35 @@ public class CustomerResource {
 		
 		@GetMapping(path = "/", produces = "application/json")
 		public List<Customer> getAllCustomers(){
-			//CustomerService customerService= new CustomerService();
 			System.out.print("hello");
 			return customerService.findAllCustomers();
 			
 		}
+		@GetMapping(path = "/{customernumber}", produces = "application/json")
+		public Optional<Customer> getCustomer(@PathVariable int customernumber){
+			System.out.print("get customer");
+			return customerService.getCustomer(customernumber);
+			
+		}
+		@PostMapping(path = "/", consumes = "application/json", produces = "application/json")
+		public Customer insertCustomer(@RequestBody Customer customer){
+			System.out.print("hello");
+			return customerService.insertCustomer(customer);
+		}
+		@PutMapping("/")
+		public Customer updateCustomer(@RequestBody Customer customer){
+			System.out.print("hello");
+			return customerService.updateCustomer(customer);
+		}
+		
+		@DeleteMapping(path = "/{customernumber}", produces = "application/json")
+		public void deleteCustomer(@PathVariable int customernumber){
+			System.out.print("get customer");
+			 customerService.deleteCustomer(customernumber);	
+		}
 		
 		@RequestMapping(value= "/test",method = RequestMethod.GET)
 		public String test() {
-		//	System.out.println(productServiceImpl.findAll());
 	        return "hello";
 	    }
 }
--- src/main/java/com/demo/crud/service/CustomerService.java
+++ src/main/java/com/demo/crud/service/CustomerService.java
@@ -2,6 +2,7 @@ package com.demo.crud.service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -19,5 +20,25 @@ public class CustomerService {
 		List<Customer> customers = new ArrayList<Customer>();  
 		customerRepository.findAll().forEach(customers1 -> customers.add(customers1));
 		return customers;
+	}
+
+	public Optional<Customer> getCustomer(int customernumber) {
+		// TODO Auto-generated method stub
+		return customerRepository.findById(customernumber);
+	}
+
+	public Customer insertCustomer(Customer customer) {
+		// TODO Auto-generated method stub
+		return customerRepository.save(customer);
+	}
+
+	public void deleteCustomer(int customernumber) {
+		// TODO Auto-generated method stub
+		 customerRepository.deleteById(customernumber);
+	}
+
+	public Customer updateCustomer(Customer customer) {
+		// TODO Auto-generated method stub
+		return customerRepository.save(customer);
 	}
 }
